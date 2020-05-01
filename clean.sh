b850d93bcb3d2d4cbbb846d213b20c52e8acb30b
Klaus Landsdorf <klaus.landsdorf@bianco-royal.de>
2020-05-01 15:50:43 +0200

    chore: changes to focus on issue #6

--- clean.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-rm -rf node_modules/
-
-rm package-lock.json
-
-npm i
-
-# npm run build
--- npm-update.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-npm cache verify
-
-npm outdated --depth=0
-
-npm install
