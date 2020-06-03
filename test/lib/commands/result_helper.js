43adba9f40b8df141414bdf96b3e5163d2b9cdb4
Nick O'Leary <nick.oleary@gmail.com>
2020-06-03 09:56:15 +0100

    Remove colors from output

--- test/lib/commands/result_helper.js
+++ test/lib/commands/result_helper.js
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  **/
-var colors = require("colors");
 var sinon = require("sinon");
 var result = require("../../../lib/result");
 
